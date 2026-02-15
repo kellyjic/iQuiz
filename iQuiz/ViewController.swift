db5f70429922867413f0e67593d0f862cbfa66fe
kellyjic <kellyyc@uw.edu>
2026-02-15 13:55:13 -0800

    Initial Commit

---
+++ iQuiz/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  iQuiz
+//
+//  Created by Kelly Chang on 2026/2/15.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
