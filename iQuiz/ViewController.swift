3afd48da13a031c97c66fd03e8a4631bc7fcf4dc
kellyjic <kellyyc@uw.edu>
2026-02-17 20:28:28 -0800

    Initial iQuiz Part 1 setup

--- iQuiz/ViewController.swift
+++ iQuiz/ViewController.swift
@@ -7,13 +7,61 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+struct QuizTopic {
+    let title: String
+    let description: String
+    let iconName: String
+}
 
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+            
+        tableView.delegate = self
+        tableView.dataSource = self
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return quizzes.count
     }
 
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell(style: .subtitle, reuseIdentifier: "cell")
+        
+        let quiz = quizzes[indexPath.row]
+        cell.textLabel?.text = quiz.title
+        cell.detailTextLabel?.text = quiz.description
+        cell.imageView?.image = UIImage(systemName: quiz.iconName)
+        
+        return cell
+    }
+    
+    var quizzes: [QuizTopic] = [
+        QuizTopic(title: "Mathematics",
+                  description: "Test your math skills.",
+                  iconName: "plus.circle"),
+        
+        QuizTopic(title: "Marvel Super Heroes",
+                  description: "How well do you know Marvel?",
+                  iconName: "bolt.circle"),
+        
+        QuizTopic(title: "Science",
+                  description: "Explore scientific knowledge.",
+                  iconName: "atom")
+    ]
+    
+    @IBOutlet weak var tableView: UITableView!
+    
+    @IBAction func settingsTapped(_ sender: UIBarButtonItem) {
+        let alert = UIAlertController(title: "Settings",
+                                      message: "Settings go here",
+                                      preferredStyle: .alert)
+        
+        alert.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
+        
+        present(alert, animated: true, completion: nil)
+    }
 
 }
 
